6047cddb4704d2ac8bf53d5a38882c902610e7c8
Arnoldo Silva <arnoldosilva@me.com>
2022-04-01 10:45:45 -0300

    add scss config

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import styles from './Home.scss'
 
 type Props = {}
 
 const Home = (props: Props) => {
   return (
-    <View testID="Home" >
+    <View testID="Home" style={styles.container}>
       <Text>Home</Text>
     </View>
   )
