d361dd053df1e702162275f642f825f42095724d
Arnoldo Silva <arnoldosilva@me.com>
2022-04-01 04:37:58 -0300

    remove sass loader

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import HomeStyle from './'
 
 type Props = {}
 
 const Home = (props: Props) => {
   return (
-    <View testID="Home">
+    <View testID="Home" >
       <Text>Home</Text>
     </View>
   )
